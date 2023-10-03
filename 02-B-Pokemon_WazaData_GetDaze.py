470b135b968efc1ff02da39e3876f15c4a70f4e0
zuumi <k0w8z0m6y1sh3k@gmail.com>
2023-10-04 00:47:50 +0900

    :+1: ゲージ技を含めて二次元配列で技データを取得

--- 02-B-Pokemon_WazaData_GetDaze.py
+++ 02-B-Pokemon_WazaData_GetDaze.py
@@ -6,43 +6,52 @@ STAGING_JSON = 'pokemon_data_test.json'
 PRODUCTION_JSON = 'pokemon_data.json'
 SAVE_JSON = STAGING_JSON
 
-with open(SAVE_JSON, 'r', encoding='utf-8') as f:
-    data = json.load(f)
-
-def getWaza(rows,pokemon_data):
-    pokemon_data = []
+def getWaza(rows):
     for row in rows:
-        data_row = list()
+        pokemon_data = list()
         columns = row.find_all('td')
         for column in columns:
             a_tag = column.find('a')
             if a_tag:
-                data_row.append(a_tag.text)
-        pokemon_data.append(data_row)
+                pokemon_data.append(a_tag.text)
+
     return pokemon_data
 
-# 各ポケモンのURLからデータを取得して追加
-for pokemon_id, pokemon_info in data[SAVE_JSON].items():
-    try:
-        url = pokemon_info['url']
-        response = requests.get(url)
-        html = response.text
-        soup = BeautifulSoup(html, 'html.parser')
-        tables = soup.find('div', class_='pgo_hyouka_waza').find_all('table')
-        if tables is None:
+def main():
+    with open(SAVE_JSON, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    # 各ポケモンのURLからデータを取得して追加
+    for pokemon_id, pokemon_info in data[SAVE_JSON].items():
+        try:
+            url = pokemon_info['url']
+            response = requests.get(url)
+            html = response.text
+            soup = BeautifulSoup(html, 'html.parser')
+            tables = soup.find_all('div', class_='pgo_hyouka_waza')
+            waza = list()
+            if tables is None:
+                continue
+            for i, table in enumerate(tables):
+                if i == 2 or i==3:
+                    continue
+                
+                waza.append(getWaza(table.find_all('table')))
+                
+                pokemon_info['waza'] = waza
+                print('.')
+
+            print('.')
+
+
+        except AttributeError:
+            pokemon_info['waza'] = []
             continue
-        rowsA = tables[0].find('table').find_all('tr')[1:]
-        rowsB = tables[1].find('table').find_all('tr')[1:]
-        pokemon_info['waza'] = getWaza(rowsA)
-        pokemon_info['waza'] = getWaza(rowsB)
 
-    except AttributeError:
-        pokemon_info['waza'] = []
-        continue
+    # 更新されたデータをpokemon_data.jsonに保存
+    with open(SAVE_JSON, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
+    print("JSONファイルに出力しました。")
 
-# 更新されたデータをpokemon_data.jsonに保存
-with open(SAVE_JSON, 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
-
-print("JSONファイルに出力しました。")
+if __name__ == "__main__":
+    main()
